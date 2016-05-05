ac30bb87edfc68057ba81a3b91611d79650f40aa
Emerson Lackey <emerson.lackey@gmail.com>
2016-05-05 10:44:27 -0400

    removing display component

--- app/components/fm-displays/rangeslider.js
+++
@@ -1,7 +0,0 @@
-import layout from '../../templates/components/fm-displays/rangeslider';
-import BaseDisplay from 'ember-form-master-2000/components/fm-displays/base';
-
-export default BaseDisplay.extend({
-  layout,
-  inputClasses: []
-});
